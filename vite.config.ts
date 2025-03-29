eac8250e533bad34aee10e8f2d6765917b826e8b
Cassio Zen <cassiozen@gmail.com>
2025-03-29 18:59:30 -0500

    Started tailwind migration

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [tailwindcss()],
   build: {
-    minify: 'terser',
+    minify: "terser",
     cssMinify: true,
     terserOptions: {
       compress: {
         drop_console: true,
-        drop_debugger: true
-      }
+        drop_debugger: true,
+      },
     },
     reportCompressedSize: false,
     rollupOptions: {
       output: {
-        manualChunks: undefined
-      }
-    }
-  }
-})
+        manualChunks: undefined,
+      },
+    },
+  },
+});
